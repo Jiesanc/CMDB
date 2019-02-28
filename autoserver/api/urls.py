a0260d2eb23671d3022ddf883e9838e1c2a6f153
Jiesanc <920371469@qq.com>
2019-02-28 19:07:42 +0800

    API+表结构设计

--- autoserver/api/urls.py
+++ autoserver/api/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from api import views
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('api.urls')),
+    url(r'^asset.html$', views.asset),
 ]
